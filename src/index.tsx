f6ea436a1f866cc625865b8e62fdedbdfdd0ad2b
Kim-Jaeoh <rlawodh123@hanmail.net>
2023-04-18 17:26:12 +0900

    TEST

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter>
           {/* <React.StrictMode> */}
           <App />
           {/* </React.StrictMode> */}
