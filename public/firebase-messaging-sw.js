d024017d761d608d20a76efa088ec6726f547326
Kim-Jaeoh <rlawodh123@hanmail.net>
2023-05-04 18:51:40 +0900

    TEST

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,16 +6,28 @@ importScripts(
 importScripts(
   "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
 );
+// // eslint-disable-next-line no-undef
+// importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+// // eslint-disable-next-line no-undef
+// importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FB_API_KEY,
-  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
-  projectId: process.env.REACT_APP_FB_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FB_MESSAGIN_SENDER_ID,
-  appId: process.env.REACT_APP_FB_APP_ID,
-  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
+  apiKey: "AIzaSyDuyihGQmcU5emS0e67Nd8oX0JUCynFTKc",
+  authDomain: "sheather-463c7.firebaseapp.com",
+  projectId: "sheather-463c7",
+  storageBucket: "sheather-463c7.appspot.com",
+  messagingSenderId: "748435717637",
+  appId: "1:748435717637:web:1c4c8315dc6c716402a66c",
+  measurementId: "G-559SZ36BYN",
+  databaseURL: "https://sheather-463c7-default-rtdb.firebaseio.com",
+  // apiKey: process.env.REACT_APP_FB_API_KEY,
+  // authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  // databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
+  // projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FB_MESSAGIN_SENDER_ID,
+  // appId: process.env.REACT_APP_FB_APP_ID,
+  // measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
 };
 
 // eslint-disable-next-line no-undef
